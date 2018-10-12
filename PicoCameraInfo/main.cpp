3c28f3af618049acc49dd54d8f18c89ee26e55b4
Azhar Aulia Saputra <Azhar@azhar-macpro.local>
2018-10-12 23:02:40 +0900

    Initial Commit

---
+++ PicoCameraInfo/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PicoCameraInfo
+//
+//  Created by Azhar Aulia Saputra on 10/12/18.
+//  Copyright © 2018 Azhar Aulia Saputra. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
